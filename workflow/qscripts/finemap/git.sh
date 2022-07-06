29d2779d934769358aa78f4764f8b4bf09fe0534
joreynajr <joreyna@live.com>
2022-07-06 14:18:00 -0700

    Intersecting finemap approach with PC-HiC

---
+++ workflow/qscripts/finemap/git.sh
@@ -0,0 +1,3 @@
+git rm ../../notebooks/reports/Intersect_Finemap_with_HiChIP.ipynb ../../notebooks/reports/Intersect_Finemap_with_HiChIP_V2.ipynb ../../notebooks/reports/Intersect_T1D_FineMapped_GWAS_with_PieQTLs.ipynb ../../notebooks/reports/Summary_of_FineMapping.ipynb
+
+git add	./ ../pc_hic/Javierre_Processing.ipynb ../pc_hic/Jung_Processing.ipynb
